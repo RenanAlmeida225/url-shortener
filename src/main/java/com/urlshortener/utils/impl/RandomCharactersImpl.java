8b6220a9fdc788daa0109e67071616bf79ddf994
italo renan <italorenan225@gmail.com>
2023-08-19 16:01:27 -0300

    feat: create RandomCharactersImpl

---
+++ src/main/java/com/urlshortener/utils/impl/RandomCharactersImpl.java
@@ -0,0 +1,10 @@
+package com.urlshortener.utils.impl;
+
+import com.urlshortener.utils.RandomCharacters;
+
+public class RandomCharactersImpl implements RandomCharacters {
+    @Override
+    public String generate(int size) {
+        return null;
+    }
+}
