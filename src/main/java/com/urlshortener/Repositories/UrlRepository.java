9f32a102cf6ba0019c922513237864132c5766c3
italo renan <italorenan225@gmail.com>
2023-08-19 15:39:56 -0300

    refactor: add @Repository

--- src/main/java/com/urlshortener/Repositories/UrlRepository.java
+++ src/main/java/com/urlshortener/Repositories/UrlRepository.java
@@ -2,9 +2,11 @@ package com.urlshortener.Repositories;
 
 import com.urlshortener.entities.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UrlRepository extends JpaRepository<Url, Long> {
     Optional<Url> findByShortUrl(String shortUrl);
 }
