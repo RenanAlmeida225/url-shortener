d72245a80b26f22f65f4bcd6c85a17f6ab888501
italo renan <italorenan225@gmail.com>
2023-08-19 17:00:01 -0300

    feat: add function for save url

--- src/main/java/com/urlshortener/services/impl/UrlServiceImpl.java
+++ src/main/java/com/urlshortener/services/impl/UrlServiceImpl.java
@@ -1,13 +1,30 @@
 package com.urlshortener.services.impl;
 
+import com.urlshortener.Repositories.UrlRepository;
+import com.urlshortener.entities.Url;
 import com.urlshortener.services.UrlService;
+import com.urlshortener.utils.RandomCharacters;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class UrlServiceImpl implements UrlService {
+
+    private final UrlRepository urlRepository;
+    private final RandomCharacters randomCharacters;
+    @Value("${url.domain}")
+    private String urlDomain;
+
     @Override
     public String saveUrl(String longUrl) {
-        return null;
+        String shortUrl = randomCharacters.generate(5);
+        this.urlRepository.findByShortUrl(shortUrl)
+                .orElseThrow(() -> new RuntimeException("short url exists"));
+        Url url = new Url(longUrl, shortUrl);
+        this.urlRepository.save(url);
+        return this.urlDomain + shortUrl;
     }
 
     @Override
