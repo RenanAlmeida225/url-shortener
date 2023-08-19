f58389d8e0b71bd08b70d158ada44c7152b5e9d6
italo renan <italorenan225@gmail.com>
2023-08-19 15:50:30 -0300

    feat: create UrlServiceImpl

---
+++ src/main/java/com/urlshortener/services/impl/UrlServiceImpl.java
@@ -0,0 +1,17 @@
+package com.urlshortener.services.impl;
+
+import com.urlshortener.services.UrlService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UrlServiceImpl implements UrlService {
+    @Override
+    public String saveUrl(String longUrl) {
+        return null;
+    }
+
+    @Override
+    public String findUrl(String shortUrl) {
+        return null;
+    }
+}
