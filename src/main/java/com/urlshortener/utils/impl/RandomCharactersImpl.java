ec2431d2f565fb4d8a85aa5eaec7eb46b0f4b763
italo renan <italorenan225@gmail.com>
2023-08-19 16:58:40 -0300

    feat: add function return random string

--- src/main/java/com/urlshortener/utils/impl/RandomCharactersImpl.java
+++ src/main/java/com/urlshortener/utils/impl/RandomCharactersImpl.java
@@ -1,10 +1,23 @@
 package com.urlshortener.utils.impl;
 
 import com.urlshortener.utils.RandomCharacters;
+import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
+@Component
 public class RandomCharactersImpl implements RandomCharacters {
+
+    private static final String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    private String randomString = "";
+
     @Override
     public String generate(int size) {
-        return null;
+        if(size==0) return this.randomString;
+        Random random = new Random();
+        int randomNumber = random.nextInt(characters.length());
+        String randomCharacter = String.valueOf(characters.charAt(randomNumber));
+        this.randomString += randomCharacter;
+        return generate(size-1);
     }
 }
