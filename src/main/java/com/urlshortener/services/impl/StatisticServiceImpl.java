9cc9575aad93957c403e9cc838d70b440f0c61c0
italo renan <italorenan225@gmail.com>
2023-08-20 09:56:20 -0300

    feat: create method to get amount of access

--- src/main/java/com/urlshortener/services/impl/StatisticServiceImpl.java
+++ src/main/java/com/urlshortener/services/impl/StatisticServiceImpl.java
@@ -1,7 +1,9 @@
 package com.urlshortener.services.impl;
 
 import com.urlshortener.Repositories.StatisticRepository;
+import com.urlshortener.Repositories.UrlRepository;
 import com.urlshortener.entities.Statistic;
+import com.urlshortener.entities.Url;
 import com.urlshortener.services.StatisticService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 public class StatisticServiceImpl implements StatisticService {
 
     private final StatisticRepository statisticRepository;
+    private final UrlRepository urlRepository;
 
     @Override
     public void save(Statistic statistic) {
@@ -19,6 +22,8 @@ public class StatisticServiceImpl implements StatisticService {
 
     @Override
     public int getAmountOfAccess(String shortUrl) {
-        return 0;
+        Url url = this.urlRepository.findByShortUrl(shortUrl).orElseThrow(() -> new RuntimeException("url not found"));
+        Statistic statistic = this.statisticRepository.findByUrl(url).orElseThrow(() -> new RuntimeException("statistic not found"));
+        return statistic.getAmountOfAccess();
     }
 }
