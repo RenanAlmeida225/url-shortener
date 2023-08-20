d8824902134888613d85091322615720a84fee63
italo renan <italorenan225@gmail.com>
2023-08-20 09:08:37 -0300

    feat: add new method

--- src/main/java/com/urlshortener/services/StatisticService.java
+++ src/main/java/com/urlshortener/services/StatisticService.java
@@ -1,5 +1,9 @@
 package com.urlshortener.services;
 
+import com.urlshortener.entities.Statistic;
+
 public interface StatisticService {
+    void saveStatisticService(Statistic statistic);
+
     int getAmountOfAccess(String shortUrl);
 }
--- src/main/java/com/urlshortener/services/impl/StatisticServiceImpl.java
+++ src/main/java/com/urlshortener/services/impl/StatisticServiceImpl.java
@@ -1,5 +1,6 @@
 package com.urlshortener.services.impl;
 
+import com.urlshortener.entities.Statistic;
 import com.urlshortener.services.StatisticService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,6 +8,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class StatisticServiceImpl implements StatisticService {
+    @Override
+    public void saveStatisticService(Statistic statistic) {
+
+    }
+
     @Override
     public int getAmountOfAccess(String shortUrl) {
         return 0;
