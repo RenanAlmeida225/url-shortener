95706c810398bc79cd889da1844e9abbf4a6e7a8
italo renan <italorenan225@gmail.com>
2023-09-05 16:56:49 -0300

    fix: fix min and max

--- src/main/java/com/urlshortener/dtos/SaveUrlDto.java
+++ src/main/java/com/urlshortener/dtos/SaveUrlDto.java
@@ -4,5 +4,5 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
-public record SaveUrlDto(@NotBlank String longUrl, @Min(0) @Max(10) int limitDays) {
+public record SaveUrlDto(@NotBlank String longUrl, @Min(1) @Max(30) int limitDays) {
 }
