038dea970498ffc1de5b865640cfa0c43c93b262
italo renan <italorenan225@gmail.com>
2023-09-06 09:49:07 -0300

    refactor: change return

--- src/main/java/com/urlshortener/services/UrlService.java
+++ src/main/java/com/urlshortener/services/UrlService.java
@@ -1,7 +1,9 @@
 package com.urlshortener.services;
 
+import com.urlshortener.dtos.UrlResponseDto;
+
 public interface UrlService {
-    String generateUrl(String longUrl, int limitDays);
+    UrlResponseDto generateUrl(String longUrl, int limitDays);
 
     String findUrl(String shortUrl);
 }
