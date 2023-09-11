d3c9c97f62b18f5af33f24e3b32cf1b4eafa09ef
italo renan <italorenan225@gmail.com>
2023-09-11 16:04:51 -0300

    feat: create new handle

--- src/main/java/com/urlshortener/controllers/ExceptionHandlerControllerAdvice.java
+++ src/main/java/com/urlshortener/controllers/ExceptionHandlerControllerAdvice.java
@@ -1,42 +1,57 @@
 package com.urlshortener.controllers;
 
-import com.urlshortener.exceptions.EntityNotFoundException;
-import com.urlshortener.exceptions.StandardException;
-import jakarta.servlet.http.HttpServletRequest;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Objects;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.Objects;
+import com.urlshortener.exceptions.EntityNotFoundException;
+import com.urlshortener.exceptions.FailedSendMailException;
+import com.urlshortener.exceptions.StandardException;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ExceptionHandlerControllerAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<StandardException> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpServletRequest request) {
+    public ResponseEntity<StandardException> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
+            HttpServletRequest request) {
         Object message = Arrays.stream(Objects.requireNonNull(exception.getDetailMessageArguments())).toArray()[1];
         StandardException standardException = new StandardException(
                 Instant.now(),
                 HttpStatus.BAD_REQUEST.value(),
                 "method argument not valid",
                 message.toString(),
-                request.getRequestURI()
-        );
+                request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standardException);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<StandardException> handleEntityNotFound(EntityNotFoundException exception, HttpServletRequest request) {
+    public ResponseEntity<StandardException> handleEntityNotFound(EntityNotFoundException exception,
+            HttpServletRequest request) {
         StandardException standardException = new StandardException(
                 Instant.now(),
                 HttpStatus.NOT_FOUND.value(),
                 "entity not found",
                 exception.getMessage(),
-                request.getRequestURI()
-        );
+                request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardException);
+    }
+
+    @ExceptionHandler(FailedSendMailException.class)
+    public ResponseEntity<StandardException> handleFailedSendMail(FailedSendMailException exception,
+            HttpServletRequest request) {
+        StandardException standardException = new StandardException(
+                Instant.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "send email fail",
+                exception.getMessage(),
+                request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standardException);
     }
 }
