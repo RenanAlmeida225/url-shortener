640379f9286edaee967b3b62f3f2c721e40cc344
italo renan <italorenan225@gmail.com>
2023-09-11 16:12:09 -0300

    feat: implements function send email

--- src/main/java/com/urlshortener/services/impl/EmailServiceImpl.java
+++ src/main/java/com/urlshortener/services/impl/EmailServiceImpl.java
@@ -1,16 +1,38 @@
 package com.urlshortener.services.impl;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import com.urlshortener.exceptions.FailedSendMailException;
 import com.urlshortener.services.EmailService;
 
 @Service
 public class EmailServiceImpl implements EmailService {
 
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String to;
+
+    public EmailServiceImpl(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
     @Override
     public void sendEmail(String shortUrl, String message) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sendEmail'");
+        try {
+            SimpleMailMessage email = new SimpleMailMessage();
+            email.setTo(this.to);
+            email.setSubject(shortUrl);
+            email.setText(message);
+
+            this.mailSender.send(email);
+        } catch (MailException e) {
+            throw new FailedSendMailException("fail on send email");
+        }
     }
 
 }
