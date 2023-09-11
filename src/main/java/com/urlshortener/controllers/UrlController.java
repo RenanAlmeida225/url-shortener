c69781e2d6da2d4d42d796e966c4502b3746813c
italo renan <italorenan225@gmail.com>
2023-09-11 09:00:05 -0300

    refactor: change endpoint

--- src/main/java/com/urlshortener/controllers/UrlController.java
+++ src/main/java/com/urlshortener/controllers/UrlController.java
@@ -1,13 +1,21 @@
 package com.urlshortener.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
 import com.urlshortener.dtos.SaveUrlDto;
 import com.urlshortener.dtos.UrlResponseDto;
 import com.urlshortener.services.UrlService;
+
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping
@@ -25,12 +33,11 @@ public class UrlController {
         return ResponseEntity.status(HttpStatus.CREATED).body(shortUrl);
     }
 
-    @GetMapping("url")
+    @GetMapping("url/unshortener")
     public ResponseEntity<UrlResponseDto> getOriginalUrl(@RequestParam String shortUrl) {
         UrlResponseDto url = this.urlService.findUrl(shortUrl);
         return ResponseEntity.status(HttpStatus.OK).body(url);
     }
-
 
     @GetMapping("{shortUrl}")
     public RedirectView redirect(@PathVariable String shortUrl) {
