a7c69f5120a6803334ba559c23de6339a1028074
italo renan <italorenan225@gmail.com>
2023-09-11 16:51:29 -0300

    feat: verify if url exists

--- src/main/java/com/urlshortener/services/impl/EmailServiceImpl.java
+++ src/main/java/com/urlshortener/services/impl/EmailServiceImpl.java
@@ -8,27 +8,32 @@ import org.springframework.stereotype.Service;
 
 import com.urlshortener.exceptions.FailedSendMailException;
 import com.urlshortener.services.EmailService;
+import com.urlshortener.services.UrlService;
 
 @Service
 public class EmailServiceImpl implements EmailService {
 
     private final JavaMailSender mailSender;
+    private final UrlService urlService;
 
     @Value("${spring.mail.username}")
     private String to;
+    @Value("${url.domain}")
+    private String urlDomain;
 
-    public EmailServiceImpl(JavaMailSender mailSender) {
+    public EmailServiceImpl(JavaMailSender mailSender, UrlService urlService) {
         this.mailSender = mailSender;
+        this.urlService = urlService;
     }
 
     @Override
     public void sendEmail(String shortUrl, String message) {
+        this.urlService.findUrl(shortUrl.split(this.urlDomain)[1]);
         try {
             SimpleMailMessage email = new SimpleMailMessage();
             email.setTo(this.to);
             email.setSubject(shortUrl);
             email.setText(message);
-
             this.mailSender.send(email);
         } catch (MailException e) {
             throw new FailedSendMailException("fail on send email");
