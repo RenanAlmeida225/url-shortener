8af47945d81b69cf69808f0f559e6ed3bd7cabda
italo renan <italorenan225@gmail.com>
2023-09-14 15:37:17 -0300

    feat: create function to delete urls with limit date

--- src/main/java/com/urlshortener/Repositories/UrlRepository.java
+++ src/main/java/com/urlshortener/Repositories/UrlRepository.java
@@ -1,12 +1,19 @@
 package com.urlshortener.Repositories;
 
-import com.urlshortener.entities.Url;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import com.urlshortener.entities.Url;
 
 @Repository
 public interface UrlRepository extends JpaRepository<Url, Long> {
     Optional<Url> findByShortUrl(String shortUrl);
+
+    @Modifying
+    @Query(value = "DELETE FROM urls WHERE EXTRACT(DAY FROM(limit_date - current_timestamp)) <= 0", nativeQuery = true)
+    void deleteUrlWithLimitDate();
 }
